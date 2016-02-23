5eaf14a44468a421d91341e8119baa8e20253a3e
Alexander A Swenson <aaswenson@wisc.edu>
2016-02-22 18:38:24 -0600

    fix error in Problem3

--- Problem3.py
+++ Problem3.py
@@ -1,26 +1,16 @@
-# This file defines an event log of a particle collision in a Monte Carlo Radiation transport code.
-
-# Important information tracked in this log includes, particle position, energy, scattering angle,scattering energy,pre-collision direction, pre-collision energy, type of reaction that occurred.
-
 import math
 
-particleID = 101
-particle_position = [1,2,3]
-collision_location = [0,0,0]
-
-col_log = {}
-
-def collisionlog(particleID,particle_position,collision_position):
-   
+collision_location = [1,2,3]
+particle_position = [2,4,5]
+dr=[]
+def collision(collision_location,particle_position):
     distance_prev = 0
+    for i in range(0,len(collision_location)):
+        dr.append( collision_location[i] - particle_position[i])
+        distance_prev = distance_prev +  math.sqrt(dr[i]*dr[i])
+    return distance_prev
 
-    for i in range(0,len(collision_position)):
-         dr = collision_position[i] - particle_position[i]
-         
-         distance_prev = distance_prev +  math.sqrt(dr[i]*dr[i])
-    return dr, distance_prev 
 
-dr = collisionlog(particleID,particle_position,collision_location)
+dr  = collision(collision_location,particle_position)
 print(dr)
-    
-
+#print(distance)
