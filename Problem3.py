7ef9f189da29d3bf9386c3d3ebb68af1eabfe5bd
Alexander A Swenson <aaswenson@wisc.edu>
2016-02-22 20:28:49 -0600

    add iterations to fill collision-log

--- Problem3.py
+++ Problem3.py
@@ -1,31 +1,33 @@
 import math
 import numpy
+col_log = {}		# initialize collision log
+for j in [43,44,45]:
 
-col_log = {}       #initialize collision log dictionary
-dr = []  	   #initialize dr vector to fill later
-direction = []     # initialize direction unit vector to fill later
-collision_location = [1,2,3]
-particle_position = [2,4,5]
-ID = 43
-energy = 2.1
+    
+    dr = []  	   #initialize dr vector to fill later
+    direction = []     # initialize direction unit vector to fill later
+    collision_location = [1,2,3]
+    particle_position = [2,4,5]
+    ID = j
+    energy = 2.1
 
 # this function calculates distance to particle collision
 # inputs: collision_location, particle_position
 # outputs: distance to collision, unit vector of particle direction
-def collision(ID,collision_location,particle_position,energy):
-    distance_prev = 0
-    for i in range(0,len(collision_location)):
-        dr.append( collision_location[i] - particle_position[i])
-        distance_prev = distance_prev +  math.sqrt(dr[i]*dr[i])
-        direction = numpy.divide(dr,distance_prev)
+    def collision(ID,collision_location,particle_position,energy):
+        distance_prev = 0
+        for i in range(0,len(collision_location)):
+            dr.append( collision_location[i] - particle_position[i])
+            distance_prev = distance_prev +  math.sqrt(dr[i]*dr[i])
+            direction = numpy.divide(dr,distance_prev)
         
-    energy = energy*1000000
-    Event_Log = {"distance to collision": distance_prev,"particle direction":direction,"energy":energy}
-    return Event_Log
-Event_Log = collision(ID,collision_location,particle_position,energy)
-print(Event_Log)
+        energy = energy*1000000
+        Event_Log = {"distance to collision": distance_prev,"particle direction":direction,"energy":energy}
+        return Event_Log
+    Event_Log = collision(ID,collision_location,particle_position,energy)
+    
 
-col_log.update({ID:Event_Log})
+    col_log.update({ID:Event_Log})
 print(col_log)
 
 
